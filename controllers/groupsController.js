5ef9a411f314ec72eea94e8ab40bfcebbe37f054
AmberWein <amberw.dev@gmail.com>
2025-08-20 20:23:47 +0300

    add routes for groups

--- controllers/groupsController.js
+++ controllers/groupsController.js
@@ -1,5 +1,6 @@
 const { supabase } = require('../supabaseClient')
 
+// Create group
 async function createGroup(req, res) {
   const { group_name } = req.body
   const user = req.user
@@ -27,4 +28,58 @@ async function createGroup(req, res) {
   }
 }
 
-module.exports = { createGroup }
+// Read groups (All groups the user is a member of
+async function getMyGroups(req, res) {
+  const user = req.user
+  try {
+    const { data, error } = await supabase
+      .from('groups')
+      .select('*, group_members!inner(role)')
+      .eq('group_members.user_id', user.id)
+    if (error) throw error
+
+    res.json(data)
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+}
+
+//  Update group (only a manager can)
+async function updateGroup(req, res) {
+  const { group_id } = req.params
+  const { group_name } = req.body
+
+  if (!group_name) return res.status(400).json({ error: "group_name is required" })
+
+  try {
+    const { data, error } = await supabase
+      .from('groups')
+      .update({ group_name })
+      .eq('group_id', group_id)
+      .select()
+      .single()
+    if (error) throw error
+
+    res.json(data)
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+}
+
+// Delete group (only a manager can)
+async function deleteGroup(req, res) {
+  const { group_id } = req.params
+  try {
+    const { error } = await supabase
+      .from('groups')
+      .delete()
+      .eq('group_id', group_id)
+    if (error) throw error
+
+    res.json({ success: true })
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+}
+
+module.exports = { createGroup, getMyGroups, updateGroup, deleteGroup }
