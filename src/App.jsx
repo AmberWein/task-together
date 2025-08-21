51615b7d6f7517d005f829349906988b5392d9d1
Itamar <itamarc101@gmail.com>
2025-08-22 00:29:05 +0300

    Update profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './pages/Dashboard';
 import UpdatePassword from "./pages/UpdatePassword";
+import ProfileManagement from './pages/ProfileManagement';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/update-password" element={<UpdatePassword />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile-managent" element={<ProfileManagement />} />
       </Routes>
     </Router>
   );
