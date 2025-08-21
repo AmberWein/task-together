329df0ceb6103b3fa56c7fd25a08cc0f242e1c98
Itamar <itamarc101@gmail.com>
2025-08-21 16:32:43 +0300

    folders

--- routes/groups.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const authenticateUser = require('../middleware/authMiddleware')
-const { createGroup } = require('../controllers/groupsController')
-
-// CREATE GROUP + ADD MANAGER
-router.post('/', authenticateUser, createGroup)
-
-module.exports = router
