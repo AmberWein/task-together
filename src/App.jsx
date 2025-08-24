3cdce6e3bf8a4f69ed164efebb171e05d85635cc
AmberWein <amberw.dev@gmail.com>
2025-08-24 13:31:30 +0300

    fix deployment, remove BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import { MantineProvider } from '@mantine/core';
+// import { Notifications, notifications } from "@mantine/notifications";
+
+// import Login from './components/Login';
+// import Register from './components/Register';
+// import Dashboard from './pages/Dashboard';
+
+// import HomePage from "./pages/HomePage/HomePage";
+// import ProfileManagement from './pages/ProfileManagement';
+
+// import UpdatePassword from './pages/UpdatePassword';
+
+// function App() {
+//   return (
+//     <MantineProvider withGlobalStyles withNormalizeCSS>
+//       <Notifications />
+
+//       <Router basename="/task-together">
+//         <Routes>
+//           <Route path="/" element={<Navigate to="/login" />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/register" element={<Register />} />
+//           <Route path="/update-password" element={<UpdatePassword />} />
+//           <Route path="/dashboard" element={<Dashboard />} /> 
+//           <Route path="/home" element={<HomePage />} />
+//           <Route path="/profile-managent" element={<ProfileManagement />} />
+//         </Routes>
+//       </Router>
+//     </MantineProvider>
+//   );
+// }
+
+// export default App;
+
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
-import { Notifications, notifications } from "@mantine/notifications";
+import { Notifications } from "@mantine/notifications";
 
 import Login from './components/Login';
 import Register from './components/Register';
@@ -16,7 +51,7 @@ function App() {
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Notifications />
 
-      <Router basename="/task-together">
+      <Router>
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
